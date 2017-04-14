fabebb81138c6c8156e503ef27e74ba7029d4f49
Max <mschwaber@unm.edu>
2017-04-13 21:16:17 -0600

    Assignment #6

---
+++ New Unity Project/Assets/Scripts/PlayerDamage.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+public var maxHealth : float;
+private var currentHealth : float;
+public var healthSlider : UI.Slider;
+public var spawnPoint : Transform;
+public var bloodPrefab : Transform;
+
+function Start () {
+    currentHealth = maxHealth; 
+}
+function OnCollisionEnter2D (collision : Collision2D) {
+	if (collision.gameObject.tag == "Enemy") {
+		if (bloodPrefab != null) {
+			Instantiate (bloodPrefab, collision.contacts[0].point, Quaternion.identity); 
+		}
+		currentHealth -= 10.0f;
+
+		if (currentHealth <= 0) {
+			currentHealth = maxHealth;
+			healthSlider.value = 1;
+			transform.position = spawnPoint.position;
+		} else {
+			healthSlider.value = currentHealth / maxHealth;
+		}
+	}
+}
---
+++ New Unity Project/Assets/Scripts/SimpleAI.js
@@ -0,0 +1,105 @@
+﻿#pragma strict
+
+public var speed : float;
+public var gravity : float;
+public var jumpSpeed : float;
+public var wanderDelay : float;
+public var jumpDelay : float;
+public var target : Transform;
+public var attackDistance : float;
+public var targetLayer : LayerMask;
+
+private var nextMove : float;
+private var nextJump : float;
+private var verticalSpeed : float;
+private var grounded : boolean;
+private var facingRight = true;
+private var startPosition : Vector3;
+private var targetDistance : float;
+private var canAttack : boolean;
+
+function Start () {
+	grounded = true;
+	nextMove = 0;
+	nextJump = 0;
+	startPosition = transform.position;
+}
+
+function Update () {
+
+	if (transform.position.y < -30) {
+		transform.position = startPosition;
+	}
+
+	//var h = Input.GetAxis("Horizontal");
+	var h = AI_Move();
+	transform.position.x += h * speed * Time.deltaTime;
+
+	if (grounded) {
+		//if (Input.GetButton("Jump")) {
+		if (TimeToJump()) {
+			grounded = false;
+			verticalSpeed = jumpSpeed;
+		}
+	} else {		
+		verticalSpeed -= gravity * Time.deltaTime;
+		transform.position.y += verticalSpeed * Time.deltaTime;
+	}
+
+	if (h > 0 && !facingRight) {
+		Flip();
+	} else if (h < 0 && facingRight) {
+		Flip();
+	}
+}
+
+private function Flip() {
+	facingRight = !facingRight;
+    transform.localScale.x *= -1;
+}
+
+private function AI_Move() : int {
+	var direction = 0;
+	if (facingRight) { 
+		direction = 1;
+	} else { 
+		direction = -1;
+	}
+
+	// Get distance & direction to target
+	var currentDistance = Vector3.Distance(target.position, transform.position);
+	var rayDirection = target.position - transform.position;
+
+	// if we see player, move toward player
+	var hit = Physics2D.Linecast(transform.position, target.position, targetLayer.value);
+	if (hit.collider != null && currentDistance <= attackDistance)
+	{
+		canAttack = true;
+		direction = Mathf.Sign(rayDirection.x);
+		return direction;
+	} else { // If we cannot see player, then move randomly
+		canAttack = false;
+		if (Time.time > nextMove) {		
+			direction *= -1;
+			//nextMove = Time.time + wanderDelay;
+			nextMove = Time.time + Random.Range(1.0, wanderDelay);
+		}
+	}
+
+	return direction;
+}
+
+private function TimeToJump() : boolean {		
+	return false;
+}
+
+function OnCollisionStay2D(other : Collision2D) {
+	if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+		verticalSpeed = 0;
+		grounded = true;
+	}
+}
+
+function OnCollisionExit2D(other : Collision2D) {
+	grounded = false;
+}
